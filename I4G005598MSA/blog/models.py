9368b3ae09bcb00db730fd8d742a321a364679dc
wole aina <oyindamolaaina06@gmail.com>
2022-06-18 23:01:42 +0100

    Task Done

---
+++ I4G005598MSA/blog/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    Title=models.CharField(max_length=200)
+    Text=models.TextField()
+    Author=models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
+    Created_date=models.DateTimeField(auto_now_add=True)
+    Published_date=models.DateTimeField(default=datetime.today)
+
+    def _str_(self):
+        return self.Title
